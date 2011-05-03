0d03a061f681734792233df16e9b71f4fbe72747
Martin Eriksson <martin@artilect.se>
2011-05-03 15:10:59 +0200

    Trying to fix problems with dependencies.

--- config.ru
+++ config.ru
@@ -1,2 +1,5 @@
+require "bundler"
+Bundler.require
+
 require "./worldchat.rb"
 run WorldChat
--- worldchat.rb
+++ worldchat.rb
@@ -1,6 +1,4 @@
-require "rubygems"
-require "sinatra"
-require "haml"
+
 class WorldChat < Sinatra::Application
 	enable :sessions
 	$messages =[]
