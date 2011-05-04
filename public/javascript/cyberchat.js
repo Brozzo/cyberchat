e1d86668123b06068ca4b81bdbb71499dbfc4717
unknown <atle@.(none)>
2011-05-04 14:17:27 +0200

    fixed prepend instead of append

--- public/javascript/cyberchat.js
+++ public/javascript/cyberchat.js
@@ -2,7 +2,7 @@ $(document).ready (function() {
 	$("#post").submit(function(e){
 		$.post("/chat",$(this).serialize(),
 		function(response){
-			$("#message_area").append(response)
+			$("#message_area").prepend(response)
 		})
 		e.preventDefault()
 	})
