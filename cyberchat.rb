e3eaec01cb7734c31480c971d3f0ceff861050a7
unknown <Christoffer@.(none)>
2011-05-16 14:57:16 +0200

    just trying to make git push work

--- cyberchat.rb
+++ cyberchat.rb
@@ -1,9 +1,3 @@
-require "rubygems"
-require "data_mapper"
-DB = DataMapper.setup(:default, 'postgres://localhost/')
-class Post
-	include DataMapper::Resource
-end
 class CyberChat < Sinatra::Application
 	enable :sessions
 	$messages =[]
@@ -15,20 +9,24 @@ class CyberChat < Sinatra::Application
 	get "/fetch_messages" do
 		$messages.reverse.map do |m|
 		"<p>#{m}</p>"
-		end.join
+		end.join 
 	end
 		post "/chat" do
 			session[:name] = params[:name]
-			message = "#{session[:name]} says: #{params[:message]}"
-			$messages << message
+			message = "#{session[:name].capitalize} says: #{params[:message]}"
+			added_time = ""
+			hour = Time.now.hour
+			minute = Time.now.min
+			if minute == 0 or minute == 1 or minute == 2 or minute == 3 or minute == 4 or minute == 5 or minute == 6 or minute == 7 or minute == 8 or minute == 9 
+				tiden = " " + hour.to_s + ":" + "0" + minute.to_s
+			else
+				tiden = " " + hour.to_s + ":" + minute.to_s
+			end
+			$messages << message + tiden
 			"<p>#{message}</p>"
 		end
-	
+		
 	get "/style.css" do
 		sass :style 
 	end
 end
-
-
-
-'postgress://pzyfkcjkjl:tZxQ706GPZt8DkUkFD0w@ec2-50-19-113-83.compute-1.amazonaws.com/pzyfkcjkjl')
