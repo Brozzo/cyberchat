fb823f946e3fa0657967553b2fcdb367eae7b4c1
Christoffer <christoffer1996@gmail.com>
2011-09-05 15:17:47 +0200

    cleaned the code so it's easier to read and shorter

--- cyberchat.rb
+++ cyberchat.rb
@@ -16,18 +16,18 @@ class CyberChat < Sinatra::Application
 		post "/messages" do
 			session[:name] = params[:name]
 			if @@badnames.any? {|badnames| session[:name].downcase.include? badnames}
-				message = "Anonymos says: #{params[:message]}"
+				message = "Anonymous says: #{params[:message]}"
 			else
 				message = "#{session[:name].capitalize} says: #{params[:message]}"
 			end
 			hour = Time.now.hour
 			minute = Time.now.min
-			if hour == 0 or hour == 1 or hour == 2 or hour == 3 or hour == 4 or hour == 5 or hour == 6 or hour == 7 or hour == 8 or hour == 9 or hour == 10 or hour == 11 or hour == 12 or hour == 13 or hour == 14
+			if hour == 0..14
 				hour = hour + 9
-			elsif hour == 15 or hour == 16 or hour == 17 or hour == 18 or hour == 19 or hour == 20 or hour == 21 or hour == 22 or hour == 23
+			elsif hour == 15..23
 				hour = hour - 15
 			end
-			if minute == 0 or minute == 1 or minute == 2 or minute == 3 or minute == 4 or minute == 5 or minute == 6 or minute == 7 or minute == 8 or minute == 9 
+			if minute == 0..9
 				tiden = hour.to_s + ":" + "0" + minute.to_s
 			else
 				tiden = hour.to_s + ":" + minute.to_s
