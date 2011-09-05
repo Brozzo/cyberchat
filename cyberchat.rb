f32cef7132b5d5eac90c2208110f634415735fc1
Christoffer <christoffer1996@gmail.com>
2011-09-05 16:03:14 +0200

    cleaned the code so it's easier to read and shorter even more

--- cyberchat.rb
+++ cyberchat.rb
@@ -13,30 +13,30 @@ class CyberChat < Sinatra::Application
 		"<p>#{m}</p>"
 		end.join 
 	end
-		post "/messages" do
-			session[:name] = params[:name]
-			if @@badnames.any? {|badnames| session[:name].downcase.include? badnames}
-				message = "Anonymous says: #{params[:message]}"
-			else
-				message = "#{session[:name].capitalize} says: #{params[:message]}"
-			end
-			hour = Time.now.hour
-			minute = Time.now.min
-			if (0..14) === hour
-				hour = hour + 9
-			else
-				hour = hour - 15
-			end
-			if (0..9) === minute
-				tiden = hour.to_s + ":" + "0" + minute.to_s
-			else
-				tiden = hour.to_s + ":" + minute.to_s
-			end
-			if params[:message].length != 0 and session[:name].length != 0
-				$messages << message + " " + tiden
-				"<p>#{message}</p>"
-			end
+	post "/messages" do
+		session[:name] = params[:name]
+		if @@badnames.any? {|badnames| session[:name].downcase.include? badnames}
+			message = "Anonymous says: #{params[:message]}"
+		else
+			message = "#{session[:name].capitalize} says: #{params[:message]}"
 		end
+		hour = Time.now.hour
+		minute = Time.now.min
+		if (0..14) === hour
+			hour = hour + 9
+		else
+			hour = hour - 15
+		end
+		if (0..9) === minute
+			tiden = hour.to_s + ":" + "0" + minute.to_s
+		else
+			tiden = hour.to_s + ":" + minute.to_s
+		end
+		if params[:message].length != 0 and session[:name].length != 0
+			$messages << message + " " + tiden
+			"<p>#{message}</p>"
+		end
+	end
 	get "/style.css" do
 		sass :style 
 	end
