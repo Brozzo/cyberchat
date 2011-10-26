857fe0781daa44a441529b58fa826e04daa672e2
Brozzo <christoffer1996@gmail.com>
2011-10-26 23:43:08 +0300

    Update cyberchat.rb

--- cyberchat.rb
+++ cyberchat.rb
@@ -8,25 +8,25 @@ class CyberChat < Sinatra::Application
 	set :password, 'cyberchat'
 	set :admin_username, 'Masterofthis'
 	set :admin_password, 'gosebrozz1'
-	@@random = 1
-	@@admin, @@login = [false], [false]
+	@@num = 1
+	@@admin, @@login = false, false
 	enable :sessions
 	$messages = []
 	
 	get ("/style.css") {sass :style}
 	
 	get "/" do
-		@@login[0], @@admin[0] = false, false
-		@@random = 1
+		@@login, @@admin = false, false
+		@@num = 1
 		haml :startpage
 	end
 	
 	get "/chat" do
-		if @@random == 2
-			@@admin, @@login = [false], [false]
+		if @@num == 2
+			@@admin, @@login = false, false
 			redirect "/"
-		elsif (@@login[0] or @@admin[0])
-			@@random = 2
+		elsif (@@login or @@admin)
+			@@num = 2
 		else
 			redirect "/"
 		end
@@ -41,13 +41,13 @@ class CyberChat < Sinatra::Application
 	
 	post "/login" do
 		if (params[:username] == settings.admin_username and params[:password] == settings.admin_password)
-			@@admin[0] = true
+			@@admin = true
 			redirect "/chat"
 		elsif (params[:username] == settings.username and params[:password] == settings.password)
-			@@login[0] = true
+			@@login = true
 			redirect "/chat"
 		else
-			@@login[0], @@admin[0] = false, false
+			@@login, @@admin = false, false
 			"<h1>Incorrect password!</h1>"
 		end
 		haml :login
@@ -60,7 +60,7 @@ class CyberChat < Sinatra::Application
 		command = params[:message].split
 		delete = false
 		
-		if @@admin[0]
+		if @@admin
 			if command[0].downcase == 'deletemessage'
 				if command[1].to_i != 0
 					num = command[1].to_i - 1
@@ -93,10 +93,8 @@ class CyberChat < Sinatra::Application
 		case hour; when (0..14) then hour += 9; else hour -= 15; end
 		case minute; when (0..9) then time = hour.to_s + ':' + '0' + minute.to_s; else time = hour.to_s + ':' + minute.to_s; end
 		
-		if (@@admin[0] or @@login[0])
-			if (params[:message].length != 0 and session[:name].length != 0 and not delete)
-				$messages << message + ' - ' + time
-			end
+		if (params[:message].length != 0 and session[:name].length != 0 and not delete)
+			$messages << message + ' - ' + time
 		end
 		
 		message_num = 0
