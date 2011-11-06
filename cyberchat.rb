119b923355c53f03e2a4569dd0ea1e082d0c8826
unknown <Christoffer@.(none)>
2011-11-06 12:45:41 +0100

    fixed login thing with cookies

--- cyberchat.rb
+++ cyberchat.rb
@@ -8,7 +8,6 @@ class CyberChat < Sinatra::Application
 	set :password, 'cyberchat'
 	set :admin_username, 'Masterofthis'
 	set :admin_password, 'gosebrozz1'
-	@@num = 1
 	@@admin, @@login = false, false
 	enable :sessions
 	$messages = []
@@ -16,16 +15,14 @@ class CyberChat < Sinatra::Application
 	get ("/style.css") {sass :style}
 	
 	get "/" do
+		session[:admin] = false
 		@@login, @@admin = false, false
-		@@num = 1
 		haml :startpage
 	end
 	
 	get "/chat" do
-		if @@num == 2
-			redirect "/"
-		elsif (@@login or @@admin)
-			@@num = 2
+		if (@@admin or @@login or session[:admin] == 'admin' or session[:admin] == 'user')
+			@@login, @@admin = false, false
 		else
 			redirect "/"
 		end
@@ -34,18 +31,21 @@ class CyberChat < Sinatra::Application
 	
 	get "/fetch_messages" do
 		$messages.reverse.map do |m|
-		"<p>#{m}</p>"
+			"<p>#{m}</p>"
 		end.join
 	end
 	
 	post "/login" do
 		if (params[:username] == settings.admin_username and params[:password] == settings.admin_password)
 			@@admin = true
+			session[:admin] = 'admin'
 			redirect "/chat"
 		elsif (params[:username] == settings.username and params[:password] == settings.password)
+			session[:admin] = 'user'
 			@@login = true
 			redirect "/chat"
 		else
+			session[:admin] = false
 			@@login, @@admin = false, false
 			"<h1>Incorrect password!</h1>"
 		end
@@ -55,11 +55,11 @@ class CyberChat < Sinatra::Application
 	post "/messages" do
 		
 		session[:name] = params[:name]
-		message = ': ' + params[:message]
-		command = params[:message].split
+		message = params[:message]
+		command = message.split
 		delete = false
 		
-		if @@admin
+		if session[:admin] == 'admin'
 			if command[0].downcase == 'deletemessage'
 				if command[1].to_i != 0
 					num = command[1].to_i - 1
@@ -72,32 +72,41 @@ class CyberChat < Sinatra::Application
 			end
 		end
 		
-		command.each do |x|
-			l = message.length
-			if @@badwords.any? {|badwords| x.include? badwords}
-				message = ''
-				l.times {message = message + '*'}
+		if command.any? {|word| word =~ @@badwords}
+			message = ''
+			command.each do |x|
+				l = x.length
+				if x =~ @@badwords
+					l.times {message += '*'}
+					message += ' '
+				else
+					message += "#{x} "
+				end
+				l.times {message += '*'}
 			end
 		end
 		
-		if @@badnames.any? {|badnames| session[:name].downcase.include? badnames}
-			message = "Anonymous says" + message
+		if session[:name].downcase =~ @@badnames
+			message = "Anonymous says: #{message}"
 		else
-			message = "#{session[:name]} says" + message
+			message = "#{session[:name]} says: #{message}"
 		end
 		
 		hour = Time.now.hour
 		minute = Time.now.min
 		
 		case hour; when (0..14) then hour += 9; else hour -= 15; end
-		case minute; when (0..9) then time = hour.to_s + ':' + '0' + minute.to_s; else time = hour.to_s + ':' + minute.to_s; end
+		case minute; when (0..9) then time = "#{hour}:0#{minute}"
+		else time = "#{hour}:#{minute}"; end
 		
-		if (params[:message].length != 0 and session[:name].length != 0 and not delete)
-			$messages << message + ' - ' + time
+		if (params[:message].length > 0 and session[:name].length > 0 and not delete)
+			message += " - #{time}"
+			$messages << message
 		end
 		
 		message_num = 0
 		$messages.each {message_num += 1}
-		if message_num > 40 then $messages.delete_at(0); end
+		$messages.delete_at(0) if message_num > 40
+		"<p>#{message}</p>"
 	end
 end
